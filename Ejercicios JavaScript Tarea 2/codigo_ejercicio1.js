c3f0d06c9f5d9b36735ef866a2e92f4b9699aac4
salvador11233 <salvador11233@hotmail.com>
2021-05-27 00:41:40 -0500

    Primera subida

---
+++ Ejercicios JavaScript Tarea 2/codigo_ejercicio1.js
@@ -0,0 +1,5 @@
+  /*************** Ejercicico 1 *****************/
+  /*Solo se imprime en consola el mensaje*/
+  
+  alert('Hola Mundo');
+  alert('Este es mi primer script en JavaScript');
---
+++ Ejercicios JavaScript Tarea 2/codigo_ejercicio2.js
@@ -0,0 +1,7 @@
+  /***************** Ejercicio 2 *************/
+  /*Solo se imprime en un alert el mensaje pero ahora esyta en variables*/
+
+  var mensaje1='Hola Mundo!\nQue facil es incluir \'comillas simples\' y \"comillas dobles\"';
+  var mensaje2='Este es mi primer script en JavaScript';
+  alert(mensaje1);
+  alert(mensaje2);
